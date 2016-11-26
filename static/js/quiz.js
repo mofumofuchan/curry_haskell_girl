1fac87f3ca4f21d9615df4ddf9210d9018cb9ad6
NaotoSuzuki <c0114292a2@edu.teu.ac.jp>
2016-11-27 02:58:01 +0900

    実行時のalertの処理を削除

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -4,50 +4,43 @@
 $(function() {
 	$("<img>").attr("src", "../static/img/test0.gif");
 	$("<img>").attr("src", "../static/img/test1.png");
-	
+
 	var editor = ace.edit("editor");
 	editor.setTheme("ace/theme/monokai");
 	editor.setFontSize(14);
 	editor.getSession().setMode("ace/mode/python");
 	editor.getSession().setUseWrapMode(true);
 	editor.getSession().setTabSize(2);
-	
+
 	var initSrc = 'if __name__ == "__main__":\n  #ここをうめてね！';
-	editor.setValue(initSrc); 
-		
+	editor.setValue(initSrc);
+
 	$('#back').click(function() {alert("back index pages")});
 	$('#reset').click(function() {editor.setValue(initSrc)});
 	$('#judge').click(function() {judgeSource();});
-	
+
 	function judgeSource() {
 		var src = editor.getValue();
 		var id = '0';
 		var send_data = {id:id, src:src};
 
-		alert("ためすよ！");
-		// document.getElementById("character").src="../static/img/test0.gif";
-		
 		$.ajax({
 			contentType: 'application/json',
 			type: 'POST',
 			url: "/answer/",
 			data: JSON.stringify(send_data),
 			success: function(responce_data) {
-				// alert(responce_data);
-				// console.log(responce_data);
-				
+				console.info('answer: ok');
+				console.info(responce_data);
+
 				if (responce_data['user_problem_ans'] == true) {
 					document.getElementById("phrase").textContent = "HelloWorld";
 					document.getElementById("character").src="../static/img/test0.gif";
-					alert("正解だよ！");
 				} else {
-					alert("responce: false");
+					document.getElementById("phrase").textContent = "しっぱい・・・";
 				}
 			},
 			dataType: "json"
 		});
-		
-		// document.getElementById("dance").textContent = "評価したよ！";
-		
 	}
 });
