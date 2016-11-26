35598d86a61b7aee087e09f5c156b3e7840969d2
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-11-26 17:14:33 +0900

    update db

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ drop table if exists user_ans;
 create table user_ans (
   user_id INTEGER primary key autoincrement,
   quiz_id integer not null,
-  result integer not NULL
+  quiz_ans string not null,
+  result_flag integer not null
 );
