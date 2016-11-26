b5d79ad22b5df56d292f3760afae409d2e6433d8
mofumofuchan <you_oki1@yahoo.co.jp>
2016-11-26 23:54:37 +0900

    エディタを表示させるコードをquiz.jsに移動

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,26 +1,39 @@
+/*
+ * テキストエディタと，ソースの送受信，答え合わせ
+ */
+$(function() {
+		$.getScript("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js");
+		var editor = ace.edit("editor");
+		editor.setTheme("ace/theme/monokai");
+		editor.setFontSize(14);
+		editor.getSession().setMode("ace/mode/python");
+		editor.getSession().setUseWrapMode(true);
+		editor.getSession().setTabSize(2);
 
-var src = "print('hello world');"
-var id = '0';
+		$('#judge').click(function () {alert("let's judge!")});
 
-var send_data = {id:id, src:src};
+		function my_func() {
+				var src = editor.getValue();
+				var id = '0';
+				var send_data = {id:id, src:src};
+				
+				$.ajax({
+						contentType: 'application/json',
+						type: 'POST',
+						url: "/answer/",
+						data: JSON.stringify(send_data),
+						success: function(responce_data) {
+						alert(responce_data);
+								console.log(responce_data);
+								
+								if (responce_data['user_problem_ans'] == true) {
+										alert("responce: true");
+								} else {
+										alert("responce: false");
+								}
+						},
+						dataType: "json"
+				});
 
-function my_func() {
-
-$.ajax({
-contentType: 'application/json',
-type: 'POST',
-url: "/answer/",
-data: JSON.stringify(send_data),
-success: function(responce_data) {
-alert(responce_data);
-console.log(responce_data);
-
-if (responce_data['user_problem_ans'] == true) {
-alert("responce: true");
-} else {
-alert("responce: false");
-}
-},
-dataType: "json"
-});
-};
+		}
+ });
