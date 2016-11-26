859f1a94b6e3993a6c84fb22e26aa229d8958c8d
mofumofuchan <you_oki1@yahoo.co.jp>
2016-11-27 01:03:23 +0900

    評価ボタンで画像を動かすようにした

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -2,47 +2,49 @@
  * テキストエディタと，ソースの送受信，答え合わせ
  */
 $(function() {
-		var editor = ace.edit("editor");
-		editor.setTheme("ace/theme/monokai");
-		editor.setFontSize(14);
-		editor.getSession().setMode("ace/mode/python");
-		editor.getSession().setUseWrapMode(true);
-		editor.getSession().setTabSize(2);
-
-		var initSrc = 'if __name__ == "__main__":\n  print("Hello world")';
-		editor.setValue(initSrc); 
+	var editor = ace.edit("editor");
+	editor.setTheme("ace/theme/monokai");
+	editor.setFontSize(14);
+	editor.getSession().setMode("ace/mode/python");
+	editor.getSession().setUseWrapMode(true);
+	editor.getSession().setTabSize(2);
+	
+	var initSrc = 'if __name__ == "__main__":\n  #ここをうめてね！';
+	editor.setValue(initSrc); 
 		
-		$('#back').click(function() {alert("back index pages")});
-		$('#reset').click(function() {editor.setValue(initSrc)});
-		$('#judge').click(function() {judgeSource();});
-
-		function judgeSource() {
-				var src = editor.getValue();
-				var id = '0';
-				var send_data = {id:id, src:src};
-
-				// debug
-				alert("ためすよ！");
+	$('#back').click(function() {alert("back index pages")});
+	$('#reset').click(function() {editor.setValue(initSrc)});
+	$('#judge').click(function() {judgeSource();});
+	
+	function judgeSource() {
+		var src = editor.getValue();
+		var id = '0';
+		var send_data = {id:id, src:src};
+		
+		alert("ためすよ！");
+		// document.getElementById("character").src="../static/img/test0.gif";
+		
+		$.ajax({
+			contentType: 'application/json',
+			type: 'POST',
+			url: "/answer/",
+			data: JSON.stringify(send_data),
+			success: function(responce_data) {
+				alert(responce_data);
+				console.log(responce_data);
 				
-				$.ajax({
-						contentType: 'application/json',
-						type: 'POST',
-						url: "/answer/",
-						data: JSON.stringify(send_data),
-						success: function(responce_data) {
-						alert(responce_data);
-								console.log(responce_data);
-								
-								if (responce_data['user_problem_ans'] == true) {
-										alert("responce: true");
-								} else {
-										alert("responce: false");
-								}
-						},
-						dataType: "json"
-				});
-
-				document.getElementById("dance").textContent = "評価したよ！";
-
-		}
- });
+				if (responce_data['user_problem_ans'] == true) {
+					document.getElementById("phrase").textContent = "HelloWorld";
+					document.getElementById("charactor").src="../static/img/test0.gif";
+					alert("responce: true");
+				} else {
+					alert("responce: false");
+				}
+			},
+			dataType: "json"
+		});
+		
+		// document.getElementById("dance").textContent = "評価したよ！";
+		
+	}
+});
