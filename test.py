f51f07fa286149c708d176350596ee80d1179901
wildFoxN <ninetailsosaki@gmail.com>
2016-11-26 18:20:59 +0900

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = app = Flask(__name__);
+
+@app.route('/', methods=['POST'])
+def index():
+    result = {
+        "Result": {
+            "Greeting": "s"
+        }
+    }
+    return jsonify(result)
+
+app.run()
