99891f520a137bceb880cbe962c52cc6001aace8
wildFoxN <ninetailsosaki@gmail.com>
2016-11-26 16:16:51 +0900

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+from flask import Flask, render_template
+
+app = Flask(__name__);
+
+@app.route('/')
+def index():
+   return render_template('index.html', name="test", title="hello")
+
+'''
+@app.route('/quiz', methods=['POST'])
+def quiz_paee():
+    return render_template('')
+
+@app.route('/answer')
+def quiz_answer():
+    ''' '''
+    return
+'''
+
+app.run()
