00c9b2635f188ecffbbeb0bb3aae43bd69fb9900
Kouki Arai <c011401529@edu.teu.ac.jp>
2016-11-26 17:35:44 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-#coding:utf-8
-from flask import Flask, render_template
+# coding:utf-8
+from flask import Flask, render_template, Response, request, abort
+from server.evalProblem import EvalProblem
+from functools import wraps
 
-app = Flask(__name__);
+app = Flask(__name__)
+
+
+def consumes(content_type):
+    def _consumes(function):
+        @wraps(function)
+        def __consumes(*argv, **keywords):
+            if request.headers['Content-Type'] != content_type:
+                abort(400)
+            return function(*argv, **keywords)
+
+        return __consumes
+
+    return _consumes
+
 
 @app.route('/')
 def index():
-   return render_template('index.html', name="test", title="hello")
+    return render_template('index.html', name="test", title="hello")
 
-'''
+
 @app.route('/quiz', methods=['POST'])
 def quiz_paee():
     return render_template('')
 
-@app.route('/answer')
+
+@app.route('/answer', methods=['POST'])
+@consumes
 def quiz_answer():
-    ''' '''
-    return
-'''
+    src = request.json['src']
+    flag = EvalProblem(src).eval()
+    print(flag)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(port=8080)
