1a9393df9663cc0939b05170f12e6ba5c643c634
wildFoxN <ninetailsosaki@gmail.com>
2016-11-27 03:41:01 +0900

    schema update

--- schema.sql
+++ schema.sql
@@ -1,7 +1,14 @@
 drop table if exists user_ans;
-create table user_ans (
-  user_id INTEGER primary key autoincrement,
-  quiz_id integer not null,
-  quiz_ans string not null,
-  result_flag integer not null
+create table quiz(
+  quiz_id integer primary key autoincrement,
+  quiz_name string not null,
+  quiz_text string not null,
+  quiz_hint string not null,
+  quiz_ans string not null
+);
+
+create table user (
+  user_id integer primary key autoincrement,
+  user_name stirng not null,
+  user_answers string not null
 );
