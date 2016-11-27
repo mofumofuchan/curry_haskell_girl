056bd28d0b8bd31b7968641937d833769a0192fe
wildFoxN <ninetailsosaki@gmail.com>
2016-11-27 11:39:05 +0900

    add story

--- schema.sql
+++ schema.sql
@@ -1,18 +1,23 @@
 drop table if exists user_ans;
 create table quiz(
   quiz_id integer primary key autoincrement,
-  quiz_name string not null,
-  quiz_text string not null,
-  quiz_hint string not null,
-  quiz_ans string not null
+  quiz_name text not null,
+  quiz_text text not null,
+  quiz_hint text not null,
+  quiz_ans text not null
 );
 
 create table user (
   user_id integer primary key autoincrement,
-  user_name stirng not null
+  user_name text not null
 );
 
 create table answers (
   user_id integer not null,
   answered integer not null
 );
+
+create table story (
+  quiz_section_id integer not null,
+  section_story text not null
+)
